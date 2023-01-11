b75b1991d1e4484c1e1c0f09c2a331ecdf0ba634
raskote <raskote@gmail.com>
2023-01-12 01:44:59 +0300

    kek

---
+++ main_2.js
@@ -0,0 +1,20 @@
+// let v1 = 10
+// let v2 = 10.5
+// let v3 = true
+// let v4 = false
+// let v5 = null
+// let v6 = [10, 11, 44]
+// let v7 = {"name": "Natalia", "age":25, "salary": 2000}
+// let v8 = "Hello"
+
+// console.log(v1, typeof v1)
+// console.log(v2, typeof v2)
+// console.log(v3, typeof v3)
+// console.log(v4, typeof v4)
+// console.log(v5, typeof v5)
+// console.log(v6, typeof v6)
+// console.log(v7, typeof v7)
+// console.log(v8, typeof v8)
+
+let item1 = 10
+let item2 = 20
